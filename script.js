10b13f8fb08cc5f2afa9b5f146bf552e9eb98a1e
Sergey <93101764+bboyruso@users.noreply.github.com>
2023-02-22 18:19:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-let i = 0;
-
-const checkMinMaxCounter = () => {
-  if (i < 0) {
-    i = 0;
-    alert(`0 its a minimum value`);
-  } else if (i > 10) {
-    i = 10;
-    alert(`10 its a maximum value`);
-  } else displayCounterNum();
-};
+let counter = 0;
 
 const increment = () => {
-  i++;
-  checkMinMaxCounter();
+  counter++;
+  if (counter >= 10) {
+    counter = 10;
+    showError("maximum");
+    return;
+  }
+  displayCounterNum();
 };
+
 const decrement = () => {
-  i--;
-  checkMinMaxCounter();
+  counter--;
+  if (counter < 0) {
+    counter = 0;
+    showError("minimum");
+    return;
+  }
+  displayCounterNum();
 };
+
 const reset = () => {
-  i = 0;
+  counter = 0;
   displayCounterNum();
 };
 
@@ -28,5 +30,9 @@ document.querySelector("#two").addEventListener("click", decrement);
 document.querySelector("#tree").addEventListener("click", reset);
 
 const displayCounterNum = () => {
-  document.querySelector(".output").innerHTML = i;
+  document.querySelector(".output").innerHTML = counter;
+};
+
+const showError = (string) => {
+  document.querySelector(".output").innerHTML = `${counter} - its a ${string} value`;
 };
